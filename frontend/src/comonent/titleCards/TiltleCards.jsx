150de64bfe3d25a5d084000b42aded5cc1a0165a
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2024-12-05 23:13:58 +0530

    banner done

---
+++ frontend/src/comonent/titleCards/TiltleCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TiltleCards.css';
+
+const TiltleCards = () => {
+  return (
+    <div className='titlecards'>
+        TiltleCards
+    </div>
+  )
+}
+
+export default TiltleCards;
